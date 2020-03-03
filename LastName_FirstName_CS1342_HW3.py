bcd0f58ca9334f033da97aeacb8588d3a4214d8e
DaminiGopal <damini.n.gopal@gmail.com>
2020-03-03 11:45:20 -0600

    Update LastName_FirstName_CS1342_HW3.py

--- LastName_FirstName_CS1342_HW3.py
+++ LastName_FirstName_CS1342_HW3.py
@@ -1,12 +1,12 @@
 """
 Author / s : Jesse Contreras, Damini Gopal
 Serial Number: 22, 33
-
 Due Date : March, 4th 2020
 Assignment Number 3
 Spring 2020 - CS 1342 - 251
-
 Instructor: Husain Gholoom.
+
+This program calculates and prints parking fees for a parking company
 """
 
 import sys
@@ -25,14 +25,16 @@ tot_hours = 0.0 #float value giving total time parked in hours factoring in minu
 
 fee = 0.0       #float value that is eqaul to the total amount charged for parking duriation
 
+import math
 
 def endProgram():
     print("\n\nProgram is implement by Jesse Contreras and Damini Gopal\n3-4-20")
     sys.exit()
     
 def showTotal():
+    math.floor(fee)          
     print("\nYour car has been parked for", hours, "hours and", minutes,"minutes")
-    print("\nAmount Due  =  $", format(fee,'.2f'))
+    print("\nAmount Due  =  $", int(fee),".00")
     endProgram()
     
 print("""This program calculates and prints a parking fees for parking company
@@ -60,13 +62,13 @@ if (minutes not in range(1,60)):
 tot_hours = hours + minutes/60        
 
 if (vehicle.upper() == "C"):
-    if (tot_hours <=2): fee = tot_hours
-    else: fee = 2 + (tot_hours-2)*1.75
+    if (tot_hours <=2): fee = math.floor(tot_hours)
+    else: fee = 2 + (math.floor(tot_hours)-2)*1.75
 elif (vehicle.upper() == "B"):
-    if (tot_hours <=2): fee = 3*tot_hours
-    else: fee = 6 + (tot_hours-2)*3.25
+    if (tot_hours <=2): fee = 3*math.floor(tot_hours)
+    else: fee = 6 + (math.floor(tot_hours)-2)*3.25
 elif (vehicle.upper() == "T"):
-    if (tot_hours <=2): fee = 2*tot_hours
-    else: fee = 4 + (tot_hours-2)*2.75
+    if (tot_hours <=2): fee = 2*math.floor(tot_hours)
+    else: fee = 4 + (math.floor(tot_hours)-2)*2.75
 
 showTotal()
